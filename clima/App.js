081aef841b572650603113d3ea7d9f0971c5fa48
Luiz Vinícius <viniciusadr244@gmail.com>
2022-08-21 12:30:32 -0300

    almost

--- clima/App.js
+++ clima/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native'
+import Style from './style/styles'
 
 export default function App() {
+  const apiKey = 'd9441038f6b987a8e88413eea36c17c2'
+
+  const search = async () => {
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+      .then(response => { return response.json() })
+
+  }
+
+  const extract = (result) => {
+    const { result } = data
+    const { description, icon } = data.weather[0]
+    const { temp, feels_like, temp_min, temp_max, humidity } = data.main
+    const { visibility } = data.visibility
+    const { speed, deg } = data.wind
+    const { country, sunrise, sunset } = data.sys
+    const { timezone } = data.timezone
+    const { name } = data.name
+
+    return data
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={Style.container}>
+      <View style={[Style.border, Style.container2]}>
+        <Text>{data.main}</Text>
+      </View>
+
     </View>
   );
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ clima/style/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  container2: {
+    width: '90%'
+  },
+
+  border: {
+    borderWidth: 1,
+    borderColor: 'orange',
+  }
+
+})
