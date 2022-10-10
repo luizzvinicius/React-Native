d8bf3a922a712e59c740f4831d7649143bea0c65
Luiz Vinícius <viniciusadr244@gmail.com>
2022-10-10 20:16:24 -0300

    background and title ok

--- app-todo/App.js
+++ app-todo/App.js
@@ -1,18 +1,48 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container_safe}>
+      <View style={styles.container}>
+        <View style={styles.icon_container}>
+          <Text style={styles.title}>To Do!</Text>
+          <Image
+            source={require('../app-todo/assets/tick-inside-circle.png')}
+            style={{ width: 45, height: 45 }}
+          />
+        </View>
+      </View>
+
+      <Text>
+        Icons made by <a href="https://www.flaticon.com/authors/google" title="Google"> Google </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a>
+      </Text>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  container_safe: {
+    backgroundColor: '#2592DA',
+    height: '100%',
+  },
+
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 20,
+    borderWidth: 1,
+  },
+
+  title: {
+    color: '#fff',
+    fontFamily: 'sans-serif',
+    fontSize: 40,
+  },
+
+  icon_container: {
+    flexDirection: 'row',
     justifyContent: 'center',
+    gap: 20,
+    borderWidth: 1,
   },
 })
+
+// cores 2: #FF6500
