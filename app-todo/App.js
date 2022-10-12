4678e45e0e62a915d08040bf0b1ff42f6c8858f3
Luiz Vinícius <viniciusadr244@gmail.com>
2022-10-12 14:36:39 -0300

    Container Textinput ok

--- app-todo/App.js
+++ app-todo/App.js
@@ -1,20 +1,25 @@
-import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
+import { useState, useEffect } from 'react'
+import { SafeAreaView, StyleSheet, Text, View, Image, TextInput } from 'react-native'
 
 export default function App() {
+
   return (
     <SafeAreaView style={styles.container_safe}>
-      <View style={styles.container}>
-        <View style={styles.icon_container}>
-          <Text style={styles.title}>To Do!</Text>
-          <Image
-            source={require('../app-todo/assets/tick-inside-circle.png')}
-            style={{ width: 45, height: 45 }}
-          />
-        </View>
+      <View style={styles.icon_container}>
+        <Text style={styles.title}>To Do!</Text>
+        <Image style={{ width: 45, height: 45 }}
+          source={require('../app-todo/assets/tick-inside-circle.png')}
+        />
+      </View>
+
+      <View style={styles.tarefa_container}>
+        <TextInput style={styles.text_input} placeholder='Digite o título da sua tarefa...' />
+
+
       </View>
 
       <Text>
-        Icons made by <a href="https://www.flaticon.com/authors/google" title="Google"> Google </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a>
+        Icons made by <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
       </Text>
     </SafeAreaView>
   )
@@ -24,25 +29,37 @@ const styles = StyleSheet.create({
   container_safe: {
     backgroundColor: '#2592DA',
     height: '100%',
-  },
-
-  container: {
-    padding: 20,
-    borderWidth: 1,
-  },
-
-  title: {
-    color: '#fff',
-    fontFamily: 'sans-serif',
-    fontSize: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
 
   icon_container: {
     flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
     gap: 20,
-    borderWidth: 1,
   },
-})
 
-// cores 2: #FF6500
+  title: {
+    color: '#fff',
+    fontFamily: 'sans-serif',
+    fontSize: 60,
+  },
+
+  tarefa_container: {
+    height: 160,
+    padding: 10,
+    marginTop: 30,
+    marginBottom: 10,
+    borderColor: '#FF6500',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+
+  text_input: {
+    color: '#ffff',
+    paddingLeft: 5,
+
+  },
+  
+})
