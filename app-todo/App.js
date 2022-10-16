8ce8f83b135b3c91ba928f0253dfe8ed2b669f22
Luiz Vinícius <viniciusadr244@gmail.com>
2022-10-16 10:28:51 -0300

    Refatoração

--- app-todo/App.js
+++ app-todo/App.js
@@ -1,14 +1,11 @@
 import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './src/screen/Home'
 import FormScreen from './src/screen/FormScreen'
-import Home from './src/screen/Home';
-import { NavigationContainer } from '@react-navigation/native';
-
 
 const Stack = createStackNavigator()
 
 export default function App() {
-
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }} >
--- app-todo/src/screen/Home.js
+++ app-todo/src/screen/Home.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react'
-import { AntDesign } from '@expo/vector-icons';
 import { SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity, Switch } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
 import Task from '../components/Task'
-import { Dark, Light } from '../../themes/theme';
+import { Dark, Light } from '../../themes/theme'
 import Logo from '../../assets/LogoLight.png'
 import LogoDark from '../../assets/Logo.png'
-
 
 export default function Home({ navigation, route }) {
     const props = route.params
@@ -23,27 +22,29 @@ export default function Home({ navigation, route }) {
 
     const taskDark = (value) => {
         const novaLista = [...listaTarefas]
-        novaLista.map( (tarefa) => tarefa.dark = value)
+        novaLista.map((tarefa) => tarefa.dark = value)
         setlistTarefa(novaLista)
     }
-    
-    useEffect(() => { if (props != undefined) {
 
-        const listaNova = [...listaTarefas]
-        listaNova.push(props)
-        setlistTarefa(listaNova)
+    useEffect(() => {
+        if (props != undefined) {
 
-    } }, [props])
-    
+            const listaNova = [...listaTarefas]
+            listaNova.push(props)
+            setlistTarefa(listaNova)
 
-    useEffect( () => taskDark(isSelected), [isSelected])
-    
+        }
+    }, [props])
+
+
+    useEffect(() => taskDark(isSelected), [isSelected])
+
     return (
-        <SafeAreaView style={[styles.safeArea, {backgroundColor: Theme.bgPrimary}]}>
+        <SafeAreaView style={[styles.safeArea, { backgroundColor: Theme.bgPrimary }]}>
             <View style={styles.logo_container}>
                 <Image style={styles.logo}
-                source={isSelected ? LogoDark : Logo}
-                resizeMode='stretch'
+                    source={isSelected ? LogoDark : Logo}
+                    resizeMode='stretch'
                 />
             </View>
 
@@ -53,29 +54,29 @@ export default function Home({ navigation, route }) {
                     <View style={styles.button_container2}>
                         <Text style={styles.button_text}>Dark Mode</Text>
                         <Switch
-                            trackColor={{ false: "#5d5d5d", true: "#5d5d5d" }}
-                            thumbColor={isSelected ? "#ace4aa" : "#f4f3f4"}
-                         
+                            trackColor={{ false: '#5d5d5d', true: '#5d5d5d' }}
+                            thumbColor={isSelected ? '#AF70FF' : '#FFF'}
+
                             onValueChange={() => setSelection(!isSelected)}
                             value={isSelected}
                         />
-                
+
                     </View>
                 </View>
 
                 <View style={styles.container_task}>
                     {
-                        listaTarefas.length > 0 ? listaTarefas.map((tarefa) => <Task tarefa={tarefa.tarefa} apaga={() => apagaTarefa(tarefa.tarefa)}  id={tarefa.id} dark={tarefa.dark}/>) : []
+                        listaTarefas.length > 0 ? listaTarefas.map((tarefa) => <Task tarefa={tarefa.tarefa} apaga={() => apagaTarefa(tarefa.tarefa)} id={tarefa.id} dark={tarefa.dark} />) : []
                     }
 
                 </View>
 
-                <TouchableOpacity 
-                    style={[styles.form_button, {backgroundColor: Theme.bgPrimary}]} 
-                    onPress={ () => navigation.navigate('FormScreen', {theme: Theme, lista: listaTarefas, logo: isSelected ? LogoDark : Logo, dark: isSelected} ) }>
+                <TouchableOpacity
+                    style={[styles.form_button, { backgroundColor: Theme.bgPrimary }]}
+                    onPress={() => navigation.navigate('FormScreen', { theme: Theme, lista: listaTarefas, logo: isSelected ? LogoDark : Logo, dark: isSelected })}>
 
-                        <Text style={[styles.txt_roxo, {color: Theme.textSecondary}]}>Criar Tarefa</Text>
-                        <AntDesign name='plus' size={20} color={Theme.textSecondary} />
+                    <Text style={[styles.txt_roxo, { color: Theme.textSecondary }]}>Criar Tarefa</Text>
+                    <AntDesign name='plus' size={20} color={Theme.textSecondary} />
 
                 </TouchableOpacity>
             </View>
@@ -116,7 +117,7 @@ const styles = StyleSheet.create({
     },
 
     container: {
-        backgroundColor:'#AF70FF',
+        backgroundColor: '#AF70FF',
         gap: 5,
         padding: 20,
         borderRadius: 7,
@@ -129,7 +130,7 @@ const styles = StyleSheet.create({
     },
 
     txt_roxo: {
-        color:'#AF70FF',
+        color: '#AF70FF',
         fontWeight: 'bold',
         fontSize: 15,
     },
@@ -146,7 +147,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#fff',
         borderRadius: 5,
-        color:'#AF70FF',
+        color: '#AF70FF',
         paddingLeft: 20,
         paddingRight: 20,
     },
@@ -158,6 +159,6 @@ const styles = StyleSheet.create({
         paddingRight: 5,
         gap: 10,
         overflow: 'scroll',
-        
+
     },
 })  
